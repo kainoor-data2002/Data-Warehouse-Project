3d30eda7d79046a41d322d480ee59432b24429fc
kainoor-data2002 <nakietueh3103@gmail.com>
2024-12-08 16:30:36 +0700

    update product_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,6 +1,7 @@
 SELECT 
   order_line_id AS sales_order_line_key,
-  quantity AS quantity,
-  unit_price AS unit_price,
+  stock_item_id AS product_key,
+  quantity,
+  unit_price,
   quantity*unit_price AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
