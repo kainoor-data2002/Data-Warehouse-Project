291a9f9cb40960aeffdf6ce5dfab4f7431ffa69c
kainoor-data2002 <nakietueh3103@gmail.com>
2024-12-08 17:21:04 +0700

    update

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,19 @@
+WITH 
+dim_product__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+),
+
+dim_product__rename_column AS (
+  SELECT 
+    stock_item_id AS product_key,
+    stock_item_name AS product_name,
+    brand AS brand_name
+  FROM dim_product__source
+)
+
 SELECT 
-  CAST(stock_item_id AS INTEGER) AS product_key,
-  CAST(stock_item_name AS STRING) AS product_name,
-  CAST(brand AS STRING) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  CAST(dprc.product_key AS INTEGER) AS product_key,
+  CAST(dprc.product_name AS STRING) AS product_name,
+  CAST(dprc.brand_name AS STRING) AS brand_name
+FROM dim_product__rename_column AS dprc;
