3044dd9e8237ba893d9090549e3b2d2bde902dc6
kainoor-data2002 <nakietueh3103@gmail.com>
2024-12-14 17:59:06 +0700

    dim customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,24 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH 
+dim_customer__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+),
+
+dim_customer__rename_column AS (
+  SELECT 
+    customer_id AS customer_key,
+    customer_name AS customer_name
+  FROM dim_customer__source
+),
+
+dim_customer__cast_type AS (
+  SELECT 
+    CAST(dcrc.customer_key AS INTEGER) AS customer_key,
+    CAST(dcrc.customer_name AS STRING) AS customer_name
+  FROM dim_customer__rename_column AS dcrc
+)
+
+SELECT
+  customer_key,
+  customer_name
+FROM dim_customer__cast_type
